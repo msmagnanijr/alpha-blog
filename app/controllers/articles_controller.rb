107bdc5ace5f8daaaf1153bd1bcdff7c8fe322ef
Mauricio Magnani <msmagnanijr@gmail.com>
2022-04-16 13:34:27 -0300

    adding partials
    Signed-off-by: Mauricio Magnani <msmagnanijr@gmail.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,56 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+    end
+
+    def create
+        #render plain: params[:article].inspect
+        #render plain: @article.inspect
+        @article = Article.new(article_params)
+         if @article.save
+            flash[:notice] = "Article was create successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        flash[:notice] = "Article was deleted successfully."
+        redirect_to @article
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
